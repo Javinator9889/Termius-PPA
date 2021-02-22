57c5d56bcef5f07072ca177ed7d08367c10dcade
Javinator9889 <dev@javinator9889.com>
2021-02-22 12:39:04 +0100

    LICENSE: updated copyright notice

--- lookup-server.py
+++ lookup-server.py
@@ -1,5 +1,5 @@
 #                             termius-ppa
-#                  Copyright (C) 2020 - Javinator9889
+#                  Copyright (C) 2021 - Javinator9889
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
